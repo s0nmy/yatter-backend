c34ba525fd5e13f544b93ea78d33e1bcc85aa378
Miyu Sone <79miinyann@gmail.com>
2026-08-12 18:38:07 +0900

    fix(db): yweet テーブルの id と user_id の型を修正

--- migrations/000003_create_yweets_table.up.sql
+++ migrations/000003_create_yweets_table.up.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS `yweet` (
-  `id` varchar(255) NOT NULL,
-  `user_id` varchar(255) NOT NULL UNIQUE,
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `user_id` bigint NOT NULL,
   `content` text NOT NULL,
   `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
