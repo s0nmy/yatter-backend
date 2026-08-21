8084d8632647cc97e4700bda685c18191d534553
Miyu Sone <79miinyann@gmail.com>
2026-08-21 16:32:50 +0900

    chore: fomatter

--- app/domain/repository/yweet.go
+++ app/domain/repository/yweet.go
@@ -12,5 +12,5 @@ type Yweet interface {
 	// 保存に成功した場合は保存したユーザーを返す
 	Insert(ctx context.Context, Yweet *yweet.Yweet) (*yweet.Yweet, error)
 	// timeline の取得にあたり、全件取得用を返す
-		SelectAll(ctx context.Context) ([]*yweet.Yweet, error)
+	SelectAll(ctx context.Context) ([]*yweet.Yweet, error)
 }
